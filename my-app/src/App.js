b4df0e4cca3ea9ad30c219086d2da8f442d583cf
Vashanth <vashanthvashanth8@gmail.com>
2023-12-06 17:51:42 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Name : Vashanth Prasanna D </h1>
+      <h1>Dept : IT </h1>
+      <h1>Year : 3rd Year</h1>
+      <h1>Reg no : 73772121217 </h1>
+      <h1>Area of interest : Designing </h1>
+    </div>
+  );
+}
+
+export default App;
